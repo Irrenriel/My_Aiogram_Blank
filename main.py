bbdb4e7f80e2cac024384e63d5087027e6f28189
Irrenriel <64823985+Irrenriel@users.noreply.github.com>
2020-08-30 19:45:07 +0300

    v1.0

--- main.py
+++ main.py
@@ -10,11 +10,9 @@ from bot.functions.default.start_func import start_func
 
 
 # Handlers
-def bot_start():
-    '''<<<-----   DEFAULT   ----->>>'''
-    dp.register_message_handler(start_func, commands='start')
+'''<<<-----   DEFAULT   ----->>>'''
+dp.register_message_handler(start_func, commands='start')
 
-    executor.start_polling(dp, skip_updates=True, loop=loop)
 
 if __name__ == '__main__':
-    bot_start()
+    executor.start_polling(dp, skip_updates=True, loop=loop)
